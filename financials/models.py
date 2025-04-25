21afe986124dbefb87e4ce091fffa6ef4a18b882
Dashjax <dashjax9@gmail.com>
2025-04-25 13:52:55 -0400

    Fixed uniqueness issue.

--- financials/models.py
+++ financials/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
 
     class Meta:
         verbose_name_plural = "Categories"
+        unique_together = 'user', 'name'
 
     def __str__(self):
         return self.name
