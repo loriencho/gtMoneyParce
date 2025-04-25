897cb6ba186b57b34419083464362c054049093b
Dashjax <dashjax9@gmail.com>
2025-04-25 13:49:16 -0400

    Added models for transactions and categories

--- financials/models.py
+++ financials/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    # Income or Expense
+    TRANSACTION_TYPE_CHOICES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    # User input
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Category input
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # In/Out input
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+    # Amount input
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    # Date input
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.type} of {self.amount} on {self.date} by {self.user}"
