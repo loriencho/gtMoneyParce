5135b885a6a7dd6e114cb2e4d6432fc7841616e8
Dashjax <dashjax9@gmail.com>
2025-04-25 18:18:59 -0400

    Added income/expense filtering

--- financials/views.py
+++ financials/views.py
@@ -14,14 +14,18 @@ def transactions_list(request):
     categories = Category.objects.filter(user=request.user)
     context = {}
     filter_list = []
+    inout_list = []
     for category in categories:
         if request.GET.get(category.name):
             filter_list.append(category.name)
-
+    if request.GET.get('Income'):
+        inout_list.append('income')
+    if request.GET.get('Expense'):
+        inout_list.append('expense')
     if filter_list:
-        transactions = Transaction.objects.filter(user=request.user, category__name__in=filter_list)
+        transactions = Transaction.objects.filter(user=request.user, category__name__in=filter_list, type__in=inout_list)
     else:
-        transactions = Transaction.objects.filter(user=request.user)
+        transactions = Transaction.objects.filter(user=request.user, type__in=inout_list)
 
     context['transactions'] =  transactions
     context['categories'] = categories
