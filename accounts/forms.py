0c44c5645489814cef7072827aa62bf66775bc3f
Shrestha Jain <38604236+ShresthaJain@users.noreply.github.com>
2025-04-28 18:02:47 -0400

    added email field to signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from django import forms
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -8,14 +10,26 @@ class CustomErrorList(ErrorList):
             return ''
         return mark_safe(''.join([
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
-
-
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1',
+        for fieldname in ['username', 'email','password1',
         'password2']:
             self.fields[fieldname].help_text = None
+
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form-control'}
             )
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
